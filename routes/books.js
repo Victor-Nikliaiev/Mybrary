ab046abc789577a9f2812286a4fd37a81fb1cb6e
Victor-Nikliaiev <victor.nikliaiev@gmail.com>
2020-10-08 00:27:20 +0300

    Debug EmptyLoading in Books Route 2

--- routes/books.js
+++ routes/books.js
@@ -152,13 +152,13 @@ async function renderFormPage(res, book, form, hasError = false) {
 }
 
 function saveCover(book, coverEncoded) {
-    if(coverEncoded == null) return false
+    if(coverEncoded == null) return false;
     const cover = JSON.parse(coverEncoded)
     if(cover != null && imageMimeTypes.includes(cover.type)) {
         book.coverImage = new Buffer.from(cover.data, 'base64')
         book.coverImageType = cover.type
-    }
-    return true
+        return true
+    }    
 }
 
 module.exports = router
